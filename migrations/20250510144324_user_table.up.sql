fcfac4ca1ee1915e422ab1f65ff5729221799712
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-05-10 21:59:08 +0530

    user_migration

---
+++ migrations/20250510144324_user_table.up.sql
@@ -0,0 +1,15 @@
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'user_role') THEN
+        CREATE TYPE user_role AS ENUM ('user', 'admin');
+    END IF;
+END
+$$;
+
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(254) NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    role user_role NOT NULL
+);
