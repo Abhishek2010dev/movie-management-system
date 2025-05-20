89f99ddaf60b239bf4bac3ac35b05ac5aa86e454
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-05-20 22:56:47 +0530

    reservation model

---
+++ migrations/20250520170951_create_reservation_table.sql
@@ -0,0 +1,16 @@
+-- +goose Up
+-- +goose StatementBegin
+CREATE TABLE  reservation (
+	id SERIAL PRIMARY KEY,
+	user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	showtime_id INT NOT NULL REFERENCES showtime(id) ON DELETE CASCADE,
+	reservation_time TIMESTAMP NOT NULL,
+	seat_number VARCHAR(10) NOT NULL,
+	created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+);
+-- +goose StatementEnd
+
+-- +goose Down
+-- +goose StatementBegin
+DROP TABLE reservation;
+-- +goose StatementEnd
