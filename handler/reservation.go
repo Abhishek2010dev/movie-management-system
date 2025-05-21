ef2ee94845fdfcb5a48f02d59e2e12528b253c69
Abhishek2010dev <abhishekkumar2010dev@gmail.com>
2025-05-21 09:44:12 +0530

    cancel reservation

--- handler/reservation.go
+++ handler/reservation.go
@@ -20,3 +20,15 @@ func (r *Reservation) GetAll(c fiber.Ctx) error {
 	}
 	return c.Status(fiber.StatusOK).JSON(reservation)
 }
+
+func (r *Reservation) CancelReservation(c fiber.Ctx) error {
+	id := fiber.Params[int](c, "id")
+	deletedId, err := r.repository.Delete(c.Context(), id)
+	if err != nil {
+		return err
+	}
+	if deletedId == 0 {
+		return fiber.NewError(fiber.StatusNotFound, "Reservation not found")
+	}
+	return c.SendStatus(fiber.StatusNoContent)
+}
